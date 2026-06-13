87323e9d3eab0fa0fd32e811b10ba6df35471239
Mina_Wagdy <mina_wagdy@Mina-Wagdys-Mac.local>
2026-06-13 19:30:21 +0300

    chore: initial project setup with Clean Architecture, MVVM, and Config

---
+++ Atmos/App/AtmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmosApp.swift
+//  Atmos
+//
+//  Created by Mina_Wagdy on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AtmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
